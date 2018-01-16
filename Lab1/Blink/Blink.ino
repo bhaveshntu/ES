00d9d8394aac00c83c04f82c5d668615ce822b74
Franklin Sanicharane 2016 (N0712654) <N0712654@my.ntu.ac.uk>
2018-01-16 13:54:46 +0000

    Tue, Jan 16, 2018  1:54:46 PM

--- Lab1/Blink/Blink.ino
+++ Lab1/Blink/Blink.ino
@@ -24,13 +24,18 @@
 // the setup function runs once when you press reset or power the board
 void setup() { // comment bhavesh
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(3, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);                       // wait for a second
+  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);                       // wait for a second
+ digitalWrite(3, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(200);                       // wait for a second
+  digitalWrite(3, LOW);    // turn the LED off by making the voltage LOW
+  delay(200); 
 }
